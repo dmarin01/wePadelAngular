464b410fa1edd630c5dbb2119c23d775a86be613
Víctor Rascón <vrascon.sedano89@gmail.com>
2021-05-27 17:29:43 +0200

    atención rutas en app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './Home/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
