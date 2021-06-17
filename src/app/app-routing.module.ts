c3f77090c71db902b4159469397ab12054ad5742
Víctor Rascón <vrascon.sedano89@gmail.com>
2021-06-17 13:22:47 +0200

    Commit change home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AyudaComponent } from './ayuda/ayuda.component';
 import { FormularioLoginComponent } from './formulario/formulario-login/formulario-login.component';
 import { FormularioRegistroComponent } from './formulario/formulario-registro/formulario-registro.component';
-import { GuardGuard } from './guard.guard';
 import { HomeComponent } from './home/home.component';
 import { ListProfComponent } from './Listado/list-prof/list-prof.component';
 import { ChangeImgProfileComponent } from './Usuario/change-img-profile/change-img-profile.component';
 import { ChangePasswordComponent } from './Usuario/change-password/change-password.component';
 import { FormularioProfesorComponent } from './Usuario/formulario-profesor/formulario-profesor.component';
 import { ProfileComponent } from './Usuario/profile/profile.component';
+import { GuardGuard } from './guard.guard'
 
 
 const routes: Routes = [
