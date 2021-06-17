6a7a7843ed2dac5e9c5218ac0dc2dd01eefce26a
Víctor Rascón <vrascon.sedano89@gmail.com>
2021-06-17 13:16:01 +0200

    commit - our history

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AyudaComponent } from './ayuda/ayuda.component';
 import { FormularioLoginComponent } from './formulario/formulario-login/formulario-login.component';
 import { FormularioRegistroComponent } from './formulario/formulario-registro/formulario-registro.component';
+import { GuardGuard } from './guard.guard';
 import { HomeComponent } from './home/home.component';
 import { ListProfComponent } from './Listado/list-prof/list-prof.component';
 import { ChangeImgProfileComponent } from './Usuario/change-img-profile/change-img-profile.component';
@@ -10,17 +11,18 @@ import { ChangePasswordComponent } from './Usuario/change-password/change-passwo
 import { FormularioProfesorComponent } from './Usuario/formulario-profesor/formulario-profesor.component';
 import { ProfileComponent } from './Usuario/profile/profile.component';
 
+
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
   { path: 'register', component: FormularioRegistroComponent },
   { path: 'login', component: FormularioLoginComponent },
-  { path: 'profesores', component: ListProfComponent },
-  { path: 'user/:id', component: ProfileComponent },
-  { path: 'update', component: ProfileComponent },
-  { path: 'list', component: ListProfComponent },
-  { path: 'changepw', component: ChangePasswordComponent },
-  { path: 'changeimg', component: ChangeImgProfileComponent },
-  { path: 'formteacher', component: FormularioProfesorComponent },
+  { path: 'profesores', component: ListProfComponent, canActivate: [GuardGuard] },
+  { path: 'user/:id', component: ProfileComponent, canActivate: [GuardGuard] },
+  { path: 'update', component: ProfileComponent, canActivate: [GuardGuard] },
+  { path: 'list', component: ListProfComponent, canActivate: [GuardGuard] },
+  { path: 'changepw', component: ChangePasswordComponent, canActivate: [GuardGuard] },
+  { path: 'changeimg', component: ChangeImgProfileComponent, canActivate: [GuardGuard] },
+  { path: 'formteacher', component: FormularioProfesorComponent, canActivate: [GuardGuard] },
   { path: 'help', component: AyudaComponent },
   { path: '**', component: HomeComponent },
 ];
---
+++ src/app/guard.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GuardGuard implements CanActivate {
+  constructor(private router: Router) {
+
+  }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    let token = localStorage.getItem('token');
+    if (token) {
+      return true;
+    } else {
+      console.log('no puedes pasar')
+      return this.router.navigate(['/home']);
+    }
+  }
+
+}
